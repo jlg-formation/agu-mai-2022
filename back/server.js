047442f610aaadf7ed9b5b31fcb2f4e3ad49c503
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-02 13:39:45 +0200

    dev: installed typescript

--- back/server.js
+++ back/server.js
@@ -4,14 +4,15 @@ const express = require("express");
 const serveIndex = require("serve-index");
 const app = express();
 const port = 3000;
+const wwwDir = ".";
 
 app.use((req, res, next) => {
   console.log("req: ", req.url);
   next();
 });
 
-app.use(express.static("."));
-app.use(serveIndex("."));
+app.use(express.static(wwwDir));
+app.use(serveIndex(wwwDir, { icons: true }));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
