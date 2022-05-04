5793b7f4fc697185128ed81cdca472e62a36dae9
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-04 10:21:59 +0200

    refactor: getArticles

--- front/src/app/services/article.service.ts
+++ front/src/app/services/article.service.ts
@@ -5,30 +5,42 @@ import { Article } from '../interfaces/article';
   providedIn: 'root',
 })
 export class ArticleService {
-  articles: Article[] = [
-    {
-      name: 'truc',
-      price: 2.45,
-      qty: 456,
-    },
-    {
-      name: 'bidule',
-      price: 2.45,
-      qty: 456,
-    },
-    {
-      name: 'truc',
-      price: 2.45,
-      qty: 456,
-    },
-    {
-      name: 'truc',
-      price: 2.45,
-      qty: 456,
-    },
-  ];
+  articles = this.getArticles();
   async add(article: Article) {
     this.articles.push(article);
+    this.save();
+  }
+
+  getArticles(): Article[] {
+    const str = localStorage.getItem('articles');
+    if (!str) {
+      return [
+        {
+          name: 'truc',
+          price: 2.45,
+          qty: 456,
+        },
+        {
+          name: 'bidule',
+          price: 2.45,
+          qty: 456,
+        },
+        {
+          name: 'truc',
+          price: 2.45,
+          qty: 456,
+        },
+        {
+          name: 'truc',
+          price: 2.45,
+          qty: 456,
+        },
+      ];
+    }
+    return JSON.parse(str);
+  }
+  save() {
+    localStorage.setItem('articles', JSON.stringify(this.articles));
   }
 
   constructor() {}
