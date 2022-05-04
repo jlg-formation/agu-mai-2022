6cb2ec2857f913aba6b723dfb7b02bf9a37b8e33
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-04 15:25:53 +0200

    dev: currency pipe

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import localeFr from '@angular/common/locales/fr';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
@@ -9,6 +10,9 @@ import { HomeComponent } from './routes/home/home.component';
 import { LegalComponent } from './routes/legal/legal.component';
 import { ArticleService } from './services/article.service';
 import { HttpArticleService } from './services/http-article.service';
+
+// the second parameter 'fr' is optional
+registerLocaleData(localeFr, 'fr-FR');
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LegalComponent],
