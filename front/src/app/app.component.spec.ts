d1659b5e13cc941f573fbfa827d073c8f8149595
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-05 09:47:49 +0200

    test: all tests compile

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { LayoutModule } from './layout/layout.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, LayoutModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -16,12 +17,21 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
+  it('should render Gestion Stock', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('header span')?.textContent).toContain(
       'Gestion Stock'
+    );
+  });
+
+  it('should render Mentions Legales', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('footer a')?.textContent).toContain(
+      'Mentions Légales'
     );
   });
 });
